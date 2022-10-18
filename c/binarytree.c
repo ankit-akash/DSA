38297cbea3fd4eda9d09d0b2e31f9d8e4d91a918
Ritu Rani <98016078+Riturani87@users.noreply.github.com>
2022-10-19 00:34:43 +0530

    Create binarytree.c

---
+++ c/binarytree.c
@@ -0,0 +1,25 @@
+// Tree traversal in C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int item;
+  struct node* left;
+  struct node* right;
+};
+
+// Inorder traversal
+void inorderTraversal(struct node* root) {
+  if (root == NULL) return;
+  inorderTraversal(root->left);
+  printf("%d ->", root->item);
+  inorderTraversal(root->right);
+}
+
+// Preorder traversal
+void preorderTraversal(struct node* root) {
+  if (root == NULL) return;
+  printf("%d ->", root->item);
+  preorderTraversal(root->left);
+  preorderTrav
